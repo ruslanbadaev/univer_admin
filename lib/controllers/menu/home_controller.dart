22bf9efe41937241ce501e46aed0acd9f00926f8
ruslanbadaev <r.badaeff@gmail.com>
2021-06-07 23:59:46 +0300

    fixes

--- lib/controllers/menu/home_controller.dart
+++ lib/controllers/menu/home_controller.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:univer_admin/models/menu/menu_card.dart';
+import 'package:univer_admin/models/menu/menu_model.dart';
 
 class MenuController {
   MenuController();
 
   void getter(BuildContext context) {
-    MenuCard viewModel = Provider.of<MenuCard>(context, listen: false);
+    MenuModel viewModel = Provider.of<MenuModel>(context, listen: false);
     viewModel.getter();
   }
 
   void setter(BuildContext context) {
-    MenuCard viewModel = Provider.of<MenuCard>(context, listen: false);
+    MenuModel viewModel = Provider.of<MenuModel>(context, listen: false);
     viewModel.setter();
   }
 
   void update(BuildContext context) {
-    MenuCard viewModel = Provider.of<MenuCard>(context, listen: false);
+    MenuModel viewModel = Provider.of<MenuModel>(context, listen: false);
     viewModel.update();
   }
 
   void remove(BuildContext context) {
-    MenuCard viewModel = Provider.of<MenuCard>(context, listen: false);
+    MenuModel viewModel = Provider.of<MenuModel>(context, listen: false);
     viewModel.remove();
   }
 }
