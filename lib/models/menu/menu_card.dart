345db26d44b8284c25235a8acbb020752d0e1d05
ruslanbadaev <r.badaeff@gmail.com>
2021-06-08 00:14:41 +0300

    add example models

--- lib/models/menu/menu_card.dart
+++ lib/models/menu/menu_card.dart
@@ -4,7 +4,7 @@ import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 class MenuCard {
-  final int id;
+  final String id;
   final String title;
   final String description;
   final String route;
@@ -13,7 +13,7 @@ class MenuCard {
 
   factory MenuCard.fromJson(Map<String, dynamic> json) {
     return MenuCard(
-      id: json['id'] as int,
+      id: json['id'] as String,
       title: json['title'] as String,
       description: json['description'] as String,
       route: json['route'] as String,
@@ -21,6 +21,31 @@ class MenuCard {
   }
 
   static Future<List<MenuCard>> getAllCards() async {
-    return [];
+    return [
+      MenuCard.fromJson(
+        {
+          'id': '123',
+          'title': 'Загрузить расписание',
+          'description': 'Загрузить .dbf файл с расписанием на сервер',
+          'route': 'add_dbf'
+        },
+      ),
+      MenuCard.fromJson(
+        {
+          'id': '124',
+          'title': 'Управление моделями',
+          'description': 'Создание, редактирование, удаление серверных моделей',
+          'route': 'models'
+        },
+      ),
+      MenuCard.fromJson(
+        {
+          'id': '124',
+          'title': 'Управление html страницами',
+          'description': 'Создание, редактирование, удаление статичных страниц',
+          'route': 'models'
+        },
+      ),
+    ];
   }
 }
