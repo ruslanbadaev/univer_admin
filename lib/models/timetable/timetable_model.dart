86ddeaf5ad0787d674783c695d7923b8d94e96a8
ruslanbadaev <r.badaeff@gmail.com>
2021-06-10 00:11:04 +0300

    fix timetable model

--- lib/models/timetable/timetable_model.dart
+++ lib/models/timetable/timetable_model.dart
@@ -3,23 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:univer_admin/models/timetable/timetable.dart';
 
-enum TimetableModelStatus {
-  Ended,
-  Loading,
-  Error,
-}
-
 class TimetableModel extends ChangeNotifier {
-  TimetableModelStatus _status;
-  String _errorCode;
-  String _errorMessage;
-
-  String get errorCode => _errorCode;
-  String get errorMessage => _errorMessage;
-  TimetableModelStatus get status => _status;
-
-  List<TimetableCard> _cards = [];
-  List<TimetableCard> get cards => _cards;
+  Timetable _timetable;
+  Timetable get timetable => _timetable;
 
   TimetableModel();
 
@@ -28,38 +14,19 @@ class TimetableModel extends ChangeNotifier {
   }
 
   void getter() async {
-    _status = TimetableModelStatus.Loading;
-    notifyListeners();
-
-    try {
-      _cards = await TimetableCard.getAllCards();
-      _status = TimetableModelStatus.Ended;
-    } catch (e) {
-      _errorMessage = e.toString();
-      _status = TimetableModelStatus.Error;
-    }
-
+    _timetable = Timetable();
     notifyListeners();
   }
 
   void setter() {
-    _status = TimetableModelStatus.Loading;
-    notifyListeners();
-    _status = TimetableModelStatus.Ended;
     notifyListeners();
   }
 
   void update() {
-    _status = TimetableModelStatus.Loading;
-    notifyListeners();
-    _status = TimetableModelStatus.Ended;
     notifyListeners();
   }
 
   void remove() {
-    _status = TimetableModelStatus.Loading;
-    notifyListeners();
-    _status = TimetableModelStatus.Ended;
     notifyListeners();
   }
 }
